b0837563f6caf4945d9db4981ba0f0c7c79abad3
Bahri DOGRU <bahridogru1903@gmail.com>
2023-12-16 16:29:15 +0300

    User interface created

---
+++ static/script.js
@@ -0,0 +1,36 @@
+document.getElementById('user-input').addEventListener('keydown', function (event) {
+    if (event.key === "Enter") {
+        sendMessage();
+    }
+});
+
+async function sendMessage() {
+    // Capture the input value
+    var userInput = document.getElementById('user-input').value;
+
+    // Send a POST request to Flask server with user input
+    var response = await fetch('/send_message', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ message: userInput })
+    });
+
+    var chatBox = document.getElementById('chat-box');
+    var systemMessage = document.getElementById('system-message');
+    systemMessage.style.display = "none"; // Kullanıcı yazdığı anda ekranı temizler
+
+    if (userInput.trim() !== "") {
+        chatBox.innerHTML += '<p><strong>Siz:</strong> ' + userInput + '</p>';
+
+        // Get the response from ChatGPT
+        var jsonResponse = await response.json();
+        var chatGptResponse = jsonResponse.message;
+
+        // Display the response in the chat box
+        chatBox.innerHTML += '<p><strong>HealtBot:</strong> ' + chatGptResponse + '</p>';
+
+        document.getElementById('user-input').value = '';
+    }
+}
